6cc49bf064cffa96fe0e51907c1377aa2aaa00c1
mkepes <matejkepes@gmail.com>
2021-02-17 13:49:34 +0100

    Update for elixir 1.11

--- lib/pid_file/application.ex
+++ lib/pid_file/application.ex
@@ -6,23 +6,22 @@ defmodule PidFile.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     worker =
       case Application.get_env(:pid_file, :file, nil) do
-        nil -> []
-        file when is_binary(file) -> [worker(PidFile.Worker, [[file: file]])]
+        nil ->
+          []
+
+        file when is_binary(file) ->
+          [{PidFile.Worker, [file: file]}]
+
         {:SYSTEM, env_var} when is_binary(env_var) or is_list(env_var) ->
-          case :os.getenv(env_var, nil) do
-            nil -> throw "Missing Environment Variable:  #{env_var}"
-            "" ->  throw "Missing Environment Variable:  #{env_var}"
-            '' ->  throw "Missing Environment Variable:  #{env_var}"
-            file -> [worker(PidFile.Worker, [[file: file]])]
+          case :os.getenv(env_var) do
+            false -> throw("Missing Environment Variable:  #{env_var}")
+            file -> [{PidFile.Worker, [file: file]}]
           end
       end
 
-    children =
-      worker
+    children = worker
 
     opts = [strategy: :one_for_one, name: PidFile.Supervisor]
     Supervisor.start_link(children, opts)
